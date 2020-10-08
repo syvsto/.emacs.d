9a0f66f97eb1eb0e24cbcf7006a3b9189f2ebc65
Syver Storm-Furru <syver.storm-furru@tv2.no>
2020-10-08 08:57:23 +0200

    Add gitignore, set compiler speed for gccemacs

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+(setq comp-speed 2)
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -18,15 +19,3 @@
  (expand-file-name "settings.org"
 		   user-emacs-directory))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
