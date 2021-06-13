67b6c84519102cc8c1e9be918075afafc17fb7b7
Syver <syver.storm.furru@gmail.com>
2021-06-13 12:19:54 +0200

    Disable the git branch display of feebleline, as it causes issues/slowdowns on windows

--- init.el
+++ init.el
@@ -364,6 +364,12 @@
 
 (use-package feebleline :straight t
   :config
+  (setq feebleline-msg-functions '((feebleline-line-number         :post "" :fmt "%5s")
+                                   (feebleline-column-number       :pre ":" :fmt "%-2s")
+                                   (feebleline-file-directory      :face feebleline-dir-face :post "")
+                                   (feebleline-file-or-buffer-name :face font-lock-keyword-face :post "")
+                                   (feebleline-file-modified-star  :face font-lock-warning-face :post "")
+                                   (feebleline-project-name        :align right)))
   (feebleline-mode +1))
 
 (menu-bar-mode -1)
